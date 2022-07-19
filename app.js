46ed27a91b94e7bacad581fdd666710f1ded437a
TC Penrose <tc@penrose.us>
2022-07-19 15:41:31 -0700

    Made interactive fiddle and banjo buttons

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ console.log("Hello world")
 let introField = document.querySelector(".intro-field")
 let button = document.querySelector("button")
 
-button.onclick = function(){
-    let intro = introField.value
-    let message = "Yo wassup " + intro
+const fiddle = document.querySelector('.fiddle')
+
+fiddle.onclick = function(){
+    let message = "Ur a fiddler"
     console.log(message)
     document.write(message)
 
 }
+
+const banjo = document.querySelector('.banjo')
+
+banjo.onclick = function(){
+    let message = "Ur a banjo picker"
+    console.log(message)
+    document.write(message)
+    
+}
+
