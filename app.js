a3fd951b2a13eba94988a7337211298a2ed43608
TC Penrose <tc@penrose.us>
2022-07-20 13:09:12 -0700

    Added guitar, bass, and mandolin.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ console.log("Hello world")
 let button = document.querySelector("button")
 
 const fiddle = document.querySelector('.fiddle')
+const banjo = document.querySelector('.banjo')
+const guitar = document.querySelector('.guitar')
+const bass = document.querySelector('.bass')
+const mandolin = document.querySelector('.mandolin')
 
 fiddle.onclick = function(){
     let message = "Ur a fiddler"
@@ -12,8 +16,6 @@ fiddle.onclick = function(){
     sendMessageResponseArea.textContent = message
 }
 
-const banjo = document.querySelector('.banjo')
-
 banjo.onclick = function(){
     let message = "Ur a banjo picker"
     console.log(message)
@@ -22,3 +24,25 @@ banjo.onclick = function(){
     sendMessageResponseArea.textContent = message
 }
 
+guitar.onclick = function(){
+    let message = "Ur a guitar strummer"
+    console.log(message)
+    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+
+    sendMessageResponseArea.textContent = message
+}
+
+bass.onclick = function(){
+    let message = "Ur a bass player"
+    console.log(message)
+    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+
+    sendMessageResponseArea.textContent = message
+}
+mandolin.onclick = function(){
+    let message = "Ur a mando chopper"
+    console.log(message)
+    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+
+    sendMessageResponseArea.textContent = message
+}
