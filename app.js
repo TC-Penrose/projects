d076b3f572912de7f33d78f7b0671d57fbe45583
TC Penrose <tc@penrose.us>
2022-07-20 17:42:19 -0700

    Getting the button to work only when things are chosen

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 console.log("Hello world")
 
 let button = document.querySelector("button")
-
+let usernameField = document.querySelector('.usernameField')
+let instrumentSelect = document.querySelector('select')
 const startgame = document.querySelector('.startgame')
 
+let instrument = instrumentSelect.value
+console.log(instrument)
+let username = usernameField.value
+let message = "Yo " + username + ". Now get ready to pluck the note at the right time."
+console.log(message)
+
 startgame.onclick = function(){
-    document.write ()
+    console.log(instrument)
+    console.log(username)
+    if (instrument.length > 0, username.length > 0){
+        let sendMessageResponseArea = document.querySelector('.textonscreen')
+        sendMessageResponseArea.textContent = message
+    } 
 }
