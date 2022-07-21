80777bf1a472156dd9f309342bf7cdbfd75b8b6b
TC Penrose <tc@penrose.us>
2022-07-21 14:40:51 -0700

    Made input placeholder, kapoofed screen for new stuff

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ let usernameField = document.querySelector('.usernameField')
 let instrumentSelect = document.querySelector('select')
 const startgame = document.querySelector('.startgame')
 
-let instrument = instrumentSelect.value
-console.log(instrument)
-let username = usernameField.value
-let message = "Yo " + username + ". Now get ready to pluck the note at the right time."
-console.log(message)
+//let instrument = instrumentSelect.value
+//console.log(instrument)
+//let username = usernameField.value
+//let message = "Yo " + username + ". Now get ready to pluck the note at the right time."
+//console.log(message)
 
 startgame.onclick = function(){
     let instrument = instrumentSelect.value
     console.log(instrument)
     let username = usernameField.value
-    let message = "Yo " + username + ". Now get ready to pluck the note at the right time."
-    console.log(instrument)
     console.log(username)
-    console.log(message)
-    if (instrument.length > 0, username.length > 0){
+    if (username.length > 0 && instrument.length > 0){
+        document.querySelector ('.startscreen').style.display = "none";
+        let message = "Yo " + username + ", get ready to pluck the note at the right time."
         let sendMessageResponseArea = document.querySelector('.textonscreen')
         sendMessageResponseArea.textContent = message
+        console.log(message)
     } 
 }
