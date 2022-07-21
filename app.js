d78f4ae161e0210c8e084aa72f13de4947c61cf1
TC Penrose <tc@penrose.us>
2022-07-21 15:30:03 -0700

    Got background change.

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ let usernameField = document.querySelector('.usernameField')
 let instrumentSelect = document.querySelector('select')
 const startgame = document.querySelector('.startgame')
 
-//let instrument = instrumentSelect.value
-//console.log(instrument)
-//let username = usernameField.value
-//let message = "Yo " + username + ". Now get ready to pluck the note at the right time."
-//console.log(message)
 
 startgame.onclick = function(){
     let instrument = instrumentSelect.value
@@ -18,6 +13,10 @@ startgame.onclick = function(){
     console.log(username)
     if (username.length > 0 && instrument.length > 0){
         document.querySelector ('.startscreen').style.display = "none";
+        document.body.style.background = "url(sunset.jpeg)";
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundSize = 'cover';
+        
         let message = "Yo " + username + ", get ready to pluck the note at the right time."
         let sendMessageResponseArea = document.querySelector('.textonscreen')
         sendMessageResponseArea.textContent = message
