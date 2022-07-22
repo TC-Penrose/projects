f8fede6068850e4c79a5da19d4ce66d3b6af1c04
TC Penrose <tc@penrose.us>
2022-07-22 14:25:21 -0700

    Added button for note A, figuring out animation

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ let usernameField = document.querySelector('.usernameField')
 let instrumentSelect = document.querySelector('select')
 const startgame = document.querySelector('.startgame')
 
+const nomoretext = ()=>{
+    message = ""
+    sendMessageResponseArea = document.querySelector('.textonscreen')
+    sendMessageResponseArea.textContent = message
+}
+
 const doNextThing = ()=>{
-    document.body.style.background = "url(rusty.gif)";
+    document.body.style.background = "url(rusty.GIF)";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundPosition = "center";
+    document.querySelector ('.doNextThing').style.display = "block";
     let message = "This works!"
     let sendMessageResponseArea = document.querySelector('.textonscreen')
     sendMessageResponseArea.textContent = message
+    setTimeout(nomoretext, 2000)
 }
 
 startgame.onclick = function(){
@@ -21,7 +29,7 @@ startgame.onclick = function(){
     console.log(username)
     if (username.length > 0 && instrument.length > 0){
         document.querySelector ('.startscreen').style.display = "none";
-        let message = "Yo " + username + ", get ready to pluck the note at the right time."
+        let message = "Yo " + username + ", press the 'A' key at the right time."
         let sendMessageResponseArea = document.querySelector('.textonscreen')
         sendMessageResponseArea.textContent = message
         console.log(message)
